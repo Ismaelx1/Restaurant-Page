a553c566b8fbfee02bb1c19db558bb27fe3da5ff
Ismail Askhatir <spxoki@gmail.com>
2022-04-08 14:12:49 +0000

    prep

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import './styles/main.css'
+import './styles/contact.css'
+import './styles/about.css'
+
+console.log("21212")
+
+
+
+
+
+
+
+
+function doit() {
+    const div = document.createElement('div')
+    div.classList.add('starting')
+}
+doit()
+
